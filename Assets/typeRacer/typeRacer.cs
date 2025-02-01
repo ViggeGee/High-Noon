2f0437fe99c38329670701614cd806bf2617ab2d
ViggeGee <victorgustafsson65@gmail.com>
2025-02-01 15:58:50 +0100

    jepp

--- Assets/typeRacer/typeRacer.cs
+++ Assets/typeRacer/typeRacer.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
@@ -21,8 +23,19 @@ public class typeRacer : MonoBehaviour
 
     public Transform spawnPoint; // Set a spawn point in the scene
 
+    public TextMeshProUGUI canvasText;
+
+    int completedWords = 0;
+    string playerTyped;
+    bool gameStarted = false;
+    public bool readyToShoot = false;
+
+    public AudioSource startGameSound;
+    public AudioSource finnishRaceSound;
+
     void Start()
     {
+        StartCoroutine(CountDown());
         LoadWordsFromFile();
         // Populate dictionary (Assumes prefab names are "A", "B", "C", etc.)
         foreach (Sprite texture in letterextures)
@@ -30,6 +43,51 @@ public class typeRacer : MonoBehaviour
             char letter = texture.name[0]; // Get first character from prefab name
             letterDictionary[letter] = texture;
         }
+        PickRandomWord();
+
+    }
+
+    IEnumerator CountDown()
+    {
+        yield return StartCoroutine(FadeText("3"));
+        yield return StartCoroutine(FadeText("2"));
+        yield return StartCoroutine(FadeText("1"));
+        yield return StartCoroutine(FadeText("DRAW"));
+
+        canvasText.text = "";
+        startGameSound.Play();
+        gameStarted = true;
+    }
+
+    IEnumerator FadeText(string newText)
+    {
+        // Scale down
+        for (float t = 0; t < 0.2f; t += Time.deltaTime)
+        {
+            float scale = Mathf.Lerp(1, 0, t / 0.2f);
+            canvasText.transform.localScale = new Vector3(scale, scale, scale);
+            yield return null;
+        }
+        canvasText.transform.localScale = Vector3.zero;
+
+        // Change the text
+        canvasText.text = newText;
+
+        // Scale back up
+        for (float t = 0; t < 0.3f; t += Time.deltaTime)
+        {
+            float scale = Mathf.Lerp(0, 1, t / 0.3f);
+            canvasText.transform.localScale = new Vector3(scale, scale, scale);
+            yield return null;
+        }
+        canvasText.transform.localScale = Vector3.one;
+
+        yield return new WaitForSeconds(1.5f); // Stay visible before next fade
+    }
+
+    public void PickRandomWord()
+    {
+        ClearWords();
 
         randomWord = wordsList[Random.Range(0, wordsList.Count)];
 
@@ -46,8 +104,6 @@ public class typeRacer : MonoBehaviour
 
         }
         playerInput.onValueChanged.AddListener(delegate { CheckInput(); });
-
-        playerInput.Select();
     }
 
 
@@ -69,13 +125,48 @@ public class typeRacer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(gameStarted)
         playerInput.Select();
 
+        // Check if player finished the word
+        if (playerTyped == randomWord.ToUpper())
+        {
+            Debug.Log("Correct! Word completed: " + randomWord);
+            playerInput.text = ""; // Clear input for next word
+            completedWords++;
+            if (completedWords <= 2)
+            {
+                PickRandomWord();
+            }
+            if(completedWords >= 3)
+            {
+                finnishRaceSound.Play();
+                readyToShoot = true;
+                ClearWords();
+            }
+        }
+
+    }
+
+    public void ClearWords()
+    {
+        if (charLettersInWord.Count > 0)
+        {
+            charLettersInWord.Clear();
+        }
+        if (PrefabLettersInWord.Count > 0)
+        {
+            for (int i = 0; i < PrefabLettersInWord.Count; i++)
+            {
+                PrefabLettersInWord[i].gameObject.SetActive(false);
+            }
+            PrefabLettersInWord.Clear();
+        }
     }
 
     void CheckInput()
     {
-        string playerTyped = playerInput.text.ToUpper();
+       playerTyped = playerInput.text.ToUpper();
 
         for (int i = 0; i < PrefabLettersInWord.Count; i++)
         {
@@ -98,13 +189,7 @@ public class typeRacer : MonoBehaviour
             }
         }
 
-        // Check if player finished the word
-        if (playerTyped == randomWord)
-        {
-            Debug.Log("Correct! Word completed: " + randomWord);
-            playerInput.text = ""; // Clear input for next word
-            Start(); // Restart with a new word
-        }
+        
     
     }
 
