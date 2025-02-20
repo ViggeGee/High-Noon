423a423258cea3756b72c56977c847aa2cf605b4
ViggeGee <victorgustafsson65@gmail.com>
2025-02-20 10:07:22 +0100

    AHHHHH

--- Assets/Scripts/Networking/ChallengeManager.cs
+++ Assets/Scripts/Networking/ChallengeManager.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using Unity.Netcode;
 using Unity.VisualScripting;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using static Challenge;
