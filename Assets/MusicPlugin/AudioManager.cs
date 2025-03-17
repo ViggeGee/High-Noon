ac4a0f1136ca897cb4807703ae2f19d2f18703e3
Edgar Sjöberg <110986462+EdgarSjoberg@users.noreply.github.com>
2025-03-17 12:14:32 +0100

    made audiomanager exist in scenes when loading them

--- Assets/MusicPlugin/AudioManager.cs
+++ Assets/MusicPlugin/AudioManager.cs
@@ -1,42 +1,105 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance;
 
-    public AudioSource musicSorce;
+    public AudioSource musicSource;
     public AudioSource sfxSource;
     public AudioSource ambientSource;
 
-    public static void PlayLoopedMusic(AudioClip song)
+    public SFX randomSong;
+
+    [Header("Scene Music")]
+    public AudioClip mainMenuMusic;
+
+    [Header("Scene Ambiance")]
+    public AudioClip mainMenuAmbiance;
+
+    private void Awake()
     {
-        Instance.musicSorce.clip = song;
-        Instance.musicSorce.Play();
+        musicSource.Stop();
+        sfxSource.Stop();
+        ambientSource.Stop();
+
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        //play predefined music for each scene, or choose random song
+        StartSceneMusic();
+
+        //play predefined ambiance for each scene or dont play anything at all
+        StartSceneAmbiance();
     }
 
-    public static void PlaySFX(AudioClip sfx)
+    public void StartSceneMusic()
+    {
+        switch(SceneManager.GetActiveScene().name)
+        {
+            case "MainMenu":
+                PlayLoopedMusic(mainMenuMusic);
+                break;
+            default:
+                PlayLoopedMusic(randomSong);
+                break;
+        }
+    }
+
+    public void StartSceneAmbiance()
+    {
+        switch(SceneManager.GetActiveScene().name)
+        {
+            case "MainMenu":
+                PlayLoopedAmbient(mainMenuAmbiance);
+                break;
+            default:
+                break;
+        }
+    }
+    public void PlayLoopedMusic(AudioClip song)
+    {
+        Instance.musicSource.clip = song;
+        Instance.musicSource.Play();
+    }
+
+    public void PlayLoopedMusic(SFX song)
+    {
+        int randomIndex = Random.Range(0, song.audioClips.Length);
+        PlayLoopedMusic(song.audioClips[randomIndex]);
+    }
+
+    public void PlaySFX(AudioClip sfx)
     {
         Instance.sfxSource.clip = sfx;
         Instance.sfxSource.Play();
     }
 
-    public static void PlayLoopedAmbient(AudioClip ambient)
+    public void PlayLoopedAmbient(AudioClip ambient)
     {
         Instance.ambientSource.clip = ambient;
         Instance.ambientSource.Play();
     }
 
-    public static void StopMusic()
+    public void StopMusic()
     {
-        Instance.musicSorce.Stop();
+        Instance.musicSource.Stop();
     }
 
-    public static void StopAmbient()
+    public void StopAmbient()
     {
         Instance.ambientSource.Stop();
     }
 
-    public static void PlayRandomSFX(SFX sfx)
+    public void PlayRandomSFX(SFX sfx)
     {
         int randomIndex = Random.Range(0, sfx.audioClips.Length);
         PlaySFX(sfx.audioClips[randomIndex]);
